7cc43defe762dc45fc51ad712c468ef4173c0dd2
Shadman Ahmmed <shadmanahmmed@gmail.com>
2016-05-11 20:53:20 +0600

    Add files via upload

--- lib/common/messages.py
+++ lib/common/messages.py
@@ -1,13 +1,45 @@
 
 #!/usr/bin/python
 
-print "   )|     _________________________.------,_                  _
-        _/o|_____/  ,_____________.__;__,__,__,__,_Y...:::---===````//  
-       |==========\  ;  ;  ;  ;  ; \__,__\__,_____ --__,-.\(=~\(=)\((
-                  `-----------|__,__/__,__/__/  )=))~((   '-\=(\&(~\\\
-                                \ ==== \          \\~~\\     \~~)[JW]\\
-                                `| === |           ))~~\\     ```"""=,))
-                                 | === |           |'---') 
-                                / ==== /           `====='
-                               ´------´
-"
+import os 
+import sys 
+import textwrap
+
+def title(version):
+
+	os.system('clear')	
+        print '====================================================='
+	print ' UniSec: UniVersal Network Scanner | [Version]: Beta'
+	print '====================================================='
+	print 'A Shadman Ahmmed production Copyright 2016'
+	print '====================================================='
+
+
+        print """
+                  )|     _________________________.------,_                  _
+                _/o|_____/  ,_____________.__;__,__,__,__,_Y...:::---===````//  
+               |==========\  ;  ;  ;  ;  ; \__,__\__,_____ --__,-.\(=~\(=)\((
+                          `-----------|__,__/__,__/__/  )=))~((   '-\=(\&(~\\\
+                                        \ ==== \          \\~~\\     \~~)[JW]\\
+                                        `| === |           ))~~\\     ```"""=,))
+                                         | === |           |'---') 
+                                        / ==== /           `====='
+                                       ´------´
+
+        """
+
+def wrap_string (data, width=40, indent=32, indentAll=False, followingHeader=None):
+	
+ 	"""
+	Here it is printing an option description message in a nicely wrapped and formatted paragraph.
+
+	followingHeader -> text that also goes on the first line
+
+
+	"""
+
+
+
+
+
+
