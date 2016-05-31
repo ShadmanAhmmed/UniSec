5e22c7014496742ea83c2711249eb23cf1e24ee8
Shadman Ahmmed <shadmanahmmed@gmail.com>
2016-05-31 17:45:17 +0600

    Update UniSec.py
    Made it look more awesome on the terminal.

--- UniSec.py
+++ UniSec.py
@@ -28,4 +28,16 @@ print """
                   	    Script by Shadman Ahmmed
                      	    Version : Beta
 """
+print "Type your network/localhost Default Gateway . For example:,"
+print "192.128.0.1 "
+print "You can type 'ifcomfig' on your terminal for getting your localhost IP and if your IP is 192.128.0.4",
+print "then you have to replace that 4 with 1 which is your Default Gateway."
+print "Please enter your network[localhost] IP:"
+ip = raw_input()
+def get_nmap(options, ip):
+    command = "nmap " + options + " " + ip
+    process = os.popopen(command)
+    results = str(process.read())
+    return results
+print (get_nmap('-Sv',  ))
 
